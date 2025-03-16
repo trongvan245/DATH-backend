c594ef95504adefdbbe2dbfc6100e2237e46d717
Bui Trong Van <trongvan159357@gmail.com>
2025-03-16 23:56:23 +0700

    feature: add swagger api document

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,10 +1,35 @@
-export interface LoginDto {
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, isNotEmpty, IsNotEmpty, IsString } from "class-validator";
+
+export class LoginDto {
+  @ApiProperty({
+    example: "trongvan245",
+  })
+  @IsNotEmpty({ message: "Username is required" })
+  @IsString({ message: "Username must be a string" })
   username: string;
+
+  @ApiProperty({
+    example: "123456",
+  })
+  @IsNotEmpty({ message: "Password is required" })
+  @IsString({ message: "Password must be a string" })
   password: string;
 }
 
-export interface RegisterDto {
+export class RegisterDto {
+  @ApiProperty()
+  @IsNotEmpty({ message: "Username is required" })
+  @IsString({ message: "Username must be a string" })
   username: string;
+
+  @ApiProperty()
+  @IsEmail()
+  @IsNotEmpty({ message: "Email is required" })
   email: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   password: string;
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from "@nestjs/common";
+import { ApiTags } from "@nestjs/swagger";
 import { GetUser } from "src/common/decorators/get-user.decorator";
 import { JwtPayLoad } from "src/common/model/jwt.payload";
 
+@ApiTags("User")
 @Controller("user")
 export class UserController {
   @Get()

c9e6cff2c255893bf78533e4c9532cece99e0b93
Bui Trong Van <trongvan159357@gmail.com>
2025-04-21 22:05:00 +0700

    add power data

---
+++ prisma/migrations/20250421145739_add_field_in_device/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE `device` ADD COLUMN `monday` DOUBLE NULL,
+    ADD COLUMN `thursday` DOUBLE NULL,
+    ADD COLUMN `tuesday` DOUBLE NULL,
+    ADD COLUMN `wednesday` DOUBLE NULL;
---
+++ src/power-module/power-module.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('power-module')
+export class PowerModuleController {}
